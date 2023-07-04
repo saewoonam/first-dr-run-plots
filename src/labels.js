36a6e27b88bb5242cebfc97b6e39a45a1414ee63
Sae Woo Nam <nams@nist.gov>
2023-07-04 17:07:08 -0600

    Initial commit

---
+++ src/labels.js
@@ -0,0 +1,7 @@
+var labels = [ 'TIME', '4pumpA', '3pumpA', '4switchA', '3switchA', '4pumpB',
+    '3pumpB', '4switchB', '3switchB', '4K', '40K', '4PUMPA_HEATER_DAC',
+    '3PUMPA_HEATER_DAC', '4PUMPB_HEATER_DAC', '3PUMPB_HEATER_DAC',
+    '4SWITCHA_HEATER', '3SWITCHA_HEATER', '4SWITCHB_HEATER', '3SWITCHB_HEATER',
+    'STILL', 'mc', 'still_t', 'condenser', '3headA', '4headA', '3headB',
+    '4headB' ]
+exports.labels_fixed = labels; 
